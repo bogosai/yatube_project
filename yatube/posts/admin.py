20d1d3b3cc5f9794da1c15dbe83dd80d7f766743
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-07-24 23:09:31 +0300

    added folder with static fromm previous attempt and relpaced the DB file

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    list_editable = ('group',)
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title', 'description',)
+    empty_value_display = '-пусто-'
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+
+
+admin.site.register(Post, PostAdmin)
+
+
+admin.site.register(Group, GroupAdmin)
