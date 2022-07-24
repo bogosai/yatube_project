c4412753a214cfc655cf05cd7dae27744d89124a
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-07-24 23:03:15 +0300

    first make migrations after starting from scratch

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+
+from .models import Group, Post
 
 # Create your views here.
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    group = Group.objects.get(title=group)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+def group_list(request):
+    return HttpResponse("list of groups")
